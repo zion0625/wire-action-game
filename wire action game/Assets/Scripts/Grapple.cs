63446f36719264966659e7cd6e36655c28a03a2e
zion <jd060625@naver.com>
2023-12-29 11:52:35 +0900

    edit hook

--- wire action game/Assets/Scripts/Grapple.cs
+++ wire action game/Assets/Scripts/Grapple.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Grapple : MonoBehaviour {
     [SerializeField] GameObject hook;
+    [SerializeField] private new Camera camera;
     private LineRenderer _lineRen;
     private DistanceJoint2D _joint;
+
+    private bool _isHooked;
+    [SerializeField] float _hookCd;
     
     void Start() {
         _lineRen = GetComponent<LineRenderer>();
@@ -17,7 +21,16 @@ public class Grapple : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !_isHooked) {
+            Vector3 mouse = camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 dir = mouse - hook.transform.position;
+            float rotZ = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            hook.transform.rotation = Quaternion.Euler(0,0,rotZ);
+            hook.SetActive(true);
+        }
+        else if (Input.GetKey(KeyCode.Mouse0)) {
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0)) {
             
         }
     }
--- wire action game/Assets/Scripts/Hook.cs
+++ wire action game/Assets/Scripts/Hook.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hook : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Hook : MonoBehaviour {
+    [SerializeField] private float speed;
+    [SerializeField] GameObject parent;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * (speed * Time.deltaTime));
     }
 }
